7098bb738b7505ab1df20b1811ad9cd2fb7a0ade
AlfaTest Gabriel <53187936+GabrielAlfaTest@users.noreply.github.com>
2020-02-15 16:36:40 -0300

    Update ++

---
+++ Assets/Scripts/ActionSceneManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionSceneManager : MonoBehaviour
+{
+    float scrollSpedd;
+    [SerializeField]
+    Material materialPrefabGround;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        scrollSpedd = -0.5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * scrollSpedd;
+        materialPrefabGround.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
