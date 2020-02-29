72f73cff278036ba3f8d1664b498e9c7830d2947
AlfaTest Gabriel <53187936+GabrielAlfaTest@users.noreply.github.com>
2020-02-29 15:46:39 -0300

    Objects Scale

--- Assets/Scripts/ActionSceneManager.cs
+++ Assets/Scripts/ActionSceneManager.cs
@@ -9,15 +9,22 @@ public class ActionSceneManager : MonoBehaviour
     private float objectPosZ = 5.35f;
 
 
+    private GameObject objetoX;
+
     [SerializeField]
     Material materialPrefabGround;
-
     [SerializeField]
     GameObject rootScene;
     [SerializeField]
     GameObject cerca;
     [SerializeField]
     GameObject canos;
+    [SerializeField]
+    GameObject nuvem;
+    [SerializeField]
+    GameObject arbusto;
+    [SerializeField]
+    GameObject pedras;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +34,8 @@ public class ActionSceneManager : MonoBehaviour
 
         InvokeRepeating("CreateCerca", 1, 0.50f);
         InvokeRepeating("CreateCano", 1, 5.13f);
+        InvokeRepeating("CreateArbustoNuvemPedra", 1, 1.4f);
+
     }
 
     // Update is called once per frame
@@ -55,5 +64,56 @@ public class ActionSceneManager : MonoBehaviour
         newCano.transform.rotation = Quaternion.Euler(0, 0, 0);
         Rigidbody rb = newCano.GetComponent<Rigidbody>();
         rb.AddForce(new Vector3(0, 0, objectVel), ForceMode.Force);
+    }
+
+
+    private void CreateArbustoNuvemPedra()
+    {
+        var sortObject = Random.Range(1, 4);
+
+        var offSetNuvem = Random.Range(1.1f, 2.5f);
+        var giroRandom = Random.Range(-180.0f, 180.0f);
+        var grioNuvem = 0.0f;
+        var posX = 0.0f;
+
+        if((Random.Range(1,3)%2) == 0)
+        {
+            posX = -0.45f;
+            grioNuvem = 0.0f;
+        }
+        else
+        {
+            posX = 0.45f;
+            grioNuvem = 180.0f;
+        }
+
+
+        switch (sortObject)
+        {
+            case 1:
+                objetoX = (GameObject)Instantiate(arbusto);
+                objetoX.transform.parent = rootScene.transform;
+                objetoX.transform.position = new Vector3(posX, 0, objectPosZ);
+                objetoX.transform.rotation = Quaternion.Euler(-90, giroRandom, 0);
+                break;
+            case 2:
+                objetoX = (GameObject)Instantiate(nuvem);
+                objetoX.transform.parent = rootScene.transform;
+                objetoX.transform.position = new Vector3(posX, offSetNuvem, objectPosZ);
+                objetoX.transform.rotation = Quaternion.Euler(-90, giroRandom, 0);
+                break;
+            case 3:
+                objetoX = (GameObject)Instantiate(pedras);
+                objetoX.transform.parent = rootScene.transform;
+                objetoX.transform.position = new Vector3(posX, 0, objectPosZ);
+                objetoX.transform.rotation = Quaternion.Euler(-90, giroRandom, 0);
+                break;
+            default:
+                break;
+                
+        }
+
+        objetoX.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, objectVel), ForceMode.Force);
+
     }
 }
---
+++ Assets/Scripts/DeleteObjects.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteObjects : MonoBehaviour
+{
+    private bool cresce = false;
+    private bool diminui = false;
+    private Vector3 vectorScaleObj = new Vector3(0.035f, 0.035f, 0.035f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+        cresce = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((transform.localScale.x < 1.0f) && (cresce))
+        {
+            transform.localScale += vectorScaleObj;
+        }
+        else
+        {
+            cresce = false;
+        }
+        if (transform.position.z < -3.5f)
+        {
+            diminui = true;
+        }
+
+        if (diminui)
+        {
+            if(transform.localScale.x > 0.0f)
+            {
+                transform.localScale -= vectorScaleObj;
+            }
+            else
+            {
+                diminui = false;
+            }
+           
+        }
+
+        if (transform.position.z < -5.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
